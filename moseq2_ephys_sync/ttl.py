93e186ee5ed07799c258240cdec97f5b49926171
Jonah Pearl <jonahpearl@g.harvard.edu>
2021-12-23 17:08:05 -0500

    TTL take arbitrary leds to use

--- moseq2_ephys_sync/ttl.py
+++ moseq2_ephys_sync/ttl.py
@@ -4,11 +4,12 @@ from glob import glob
 import sync
 import pdb
 
-def ttl_workflow(base_path, save_path, num_leds, led_blink_interval, ephys_fs):
+def ttl_workflow(base_path, save_path, num_leds, leds_to_use, led_blink_interval, ephys_fs):
     """
     
     """
-    assert num_leds==4, "TTL code expects 4 LED channels, other nums of channels not yet supported"
+    # assert num_leds==4, "TTL code expects 4 LED channels, other nums of channels not yet supported"
+    assert num_leds == len(leds_to_use)
 
     # Load the TTL data
     ephys_ttl_path = glob('%s/**/TTL_*/' % base_path,recursive = True)[0]
@@ -22,8 +23,11 @@ def ttl_workflow(base_path, save_path, num_leds, led_blink_interval, ephys_fs):
     ephys_timestamps -= continuous_timestamps[0]   # subract the first timestamp from all TTLs; this way continuous ephys can safely start at 0 samples or seconds
     ephys_timestamps = ephys_timestamps / ephys_fs
 
-    ttl_channels = [-4,-3,-2,-1,1,2,3,4]
-    print('Assuming sync leds in ttl channels 1-4...')
+    # pdb.set_trace()
+
+    ttl_channels = [int(i)*sign for i in leds_to_use for sign in [-1,1]]
+    # ttl_channels = [-4,-3,-2,-1,1,2,3,4]
+    # print('Assuming sync leds in ttl channels 1-4...')
     ttl_bool = np.isin(channels, ttl_channels)
     ephys_events = np.vstack([ephys_timestamps[ttl_bool], abs(channels[ttl_bool])-1, np.sign(channels[ttl_bool])]).T
     codes, ephys_latencies = sync.events_to_codes(ephys_events, nchannels=num_leds, minCodeTime=(led_blink_interval-1))
