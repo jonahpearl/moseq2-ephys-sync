5bfcb3146975d80b20aa0b24265feea281aa2516
Jonah Pearl <jonahpearl@g.harvard.edu>
2022-02-09 14:37:13 -0500

    update basler std and roi extraction

--- moseq2_ephys_sync/extract_leds.py
+++ moseq2_ephys_sync/extract_leds.py
@@ -6,12 +6,15 @@ import os
 import numpy as np
 from skimage.feature import canny
 from scipy import ndimage as ndi
+from skimage.morphology import binary_erosion, binary_dilation, disk
 from skimage.filters import threshold_otsu, threshold_multiotsu
+from skimage.measure import regionprops
+from skimage.draw import polygon
 from moseq2_ephys_sync.plotting import plot_code_chunk, plot_matched_scatter, plot_model_errors, plot_video_frame
 import pdb
 
 
-def get_led_data_from_rois(frame_data_chunk, led_roi_list, led_thresh=2e4, save_path=None):
+def get_led_data_from_rois(frame_data_chunk, led_roi_list, movie_type, diff_thresholds=None,  save_path=None):
     """
     Given pre-determined rois for LEDs, return sequences of ons and offs
     Inputs:
@@ -25,17 +28,35 @@ def get_led_data_from_rois(frame_data_chunk, led_roi_list, led_thresh=2e4, save_
 
     leds = []
 
+    # Set up thresholds to detect LED ON/OFF events.
+    # This will be used like: np.diff(led_signal) > threshold and vice versa for off.
+    if diff_thresholds is None:
+        if movie_type == 'mkv':
+            led_thresh = 2e4
+        elif movie_type == 'avi':
+            led_thresh = None  # dynamically determine below with otsu
+        elif movie_type == 'basler':  # encoded as uint8 for now
+            led_thresh = 250
+    elif diff_thresholds is not None:
+        assert len(diff_thresholds) == len(led_roi_list)
+
     for i in range(len(led_roi_list)):
 
-
+        # Points in the frame that correspond to the LED
         pts = led_roi_list[i]
 
-        led = frame_data_chunk[:, pts[0], pts[1]].mean(axis=1) #on/off block signals  (slice returns a nframes x npts array, then you get mean of all the pts at each time)
+        # Mean frame-by-frame LED signal for this chunk
+        led = frame_data_chunk[:, pts[0], pts[1]].mean(axis=1)  # (slice returns a nframes x npts array, then you get mean of all the pts at each time)
 
+        # Specify different threshold for each LED if necessary
+        if diff_thresholds is not None:
+            led_thresh = diff_thresholds[i]
+
+        # Detect events
         led_on = np.where(np.diff(led) > led_thresh)[0]   #rise indices
         led_off = np.where(np.diff(led) < -led_thresh)[0]   #fall indices
 
-
+        # Store data
         led_vec = np.zeros(frame_data_chunk.shape[0])
         led_vec[led_on] = 1
         led_vec[led_off] = -1
@@ -43,6 +64,8 @@ def get_led_data_from_rois(frame_data_chunk, led_roi_list, led_thresh=2e4, save_
         leds.append(led_vec)
 
     leds = np.vstack(leds) #spiky differenced signals to extract times 
+    detected_count = np.sum(leds!=0, axis=1)
+    print(f'Num detected events: {detected_count}')
 
     return leds
 
@@ -59,10 +82,12 @@ def relabel_labeled_leds(labeled_led_img):
             labeled_led_img[labeled_led_img == val] = i
     return labeled_led_img
 
+
 def extract_initial_labeled_image(frames_uint8, movie_type):
     """Use std (usually) of frames and otsu thresholding to extract LED positions
     movie_type:
     """
+    
     std_px = frames_uint8.std(axis=0)    
     mean_px = frames_uint8.mean(axis=0)
     vary_px = std_px if np.std(std_px) < np.std(mean_px) else mean_px # pick the one with the lower variance
@@ -70,17 +95,30 @@ def extract_initial_labeled_image(frames_uint8, movie_type):
     # Get threshold for LEDs
     if movie_type == 'mkv':
         thresh = threshold_otsu(vary_px)
-    elif movie_type == 'avi':
+    elif movie_type == 'avi' or movie_type == 'basler':
         thresh = threshold_multiotsu(vary_px,5)[-1]  # take highest threshold from multiple
     
     # Get mask
-    thresh_px = np.copy(vary_px)
-    thresh_px[thresh_px<thresh] = 0
+    if movie_type == 'mkv' or movie_type == 'avi':
+        thresh_px = np.copy(vary_px)
+        thresh_px[thresh_px<thresh] = 0
+        
+        # Initial regions from mask
+        edges = canny(thresh_px/255.) ## find the edges
+        filled_image = ndi.binary_fill_holes(edges) ## fill its edges
+        labeled_led_img, num_features = ndi.label(filled_image) ## get the clusters
     
-    # Initial regions from mask
-    edges = canny(thresh_px/255.) ## find the edges
-    filled_image = ndi.binary_fill_holes(edges) ## fill its edges
-    labeled_led_img, num_features = ndi.label(filled_image) ## get the clusters
+    elif movie_type == 'basler':
+        thresh_px = np.copy(vary_px)
+        thresh_px = (thresh_px > thresh)
+        thresh_px = binary_erosion(thresh_px, disk(5))  # separate LEDs
+        thresh_px = binary_dilation(thresh_px)  # fix ragged edges
+
+        # Initial regions from mask
+        edges = canny(thresh_px) ## find the edges
+        filled_image = ndi.binary_fill_holes(edges) ## fill its edges
+        labeled_led_img, num_features = ndi.label(filled_image) ## get the clusters
+
 
     return num_features, filled_image, labeled_led_img
 
@@ -104,7 +142,9 @@ def clean_by_location(filled_image, labeled_led_img, led_loc):
         idx = np.asarray([(x > 0.75) for (x,y) in centers_of_mass]).nonzero()[0]
     elif led_loc == 'topquarter':
         idx = np.asarray([(x < 0.25) for (x,y) in centers_of_mass]).nonzero()[0]
-    elif led_loc == 'right_quarter':
+    elif led_loc == 'topthird':
+        idx = np.asarray([(x < 0.33) for (x,y) in centers_of_mass]).nonzero()[0]
+    elif led_loc == 'rightquarter':
         idx = np.asarray([(y > 0.75) for (x,y) in centers_of_mass]).nonzero()[0]
     elif led_loc == 'leftquarter':
         idx = np.asarray([(y < 0.25) for (x,y) in centers_of_mass]).nonzero()[0]
@@ -123,18 +163,23 @@ def clean_by_location(filled_image, labeled_led_img, led_loc):
     return labeled_led_img
 
 
-def clean_by_size(labeled_led_img, led_size_thresh):
-    
+def clean_by_size(labeled_led_img, lower_size_thresh, upper_size_thresh):
+    """Remove ROIs with num pixels less than lower thresh or greater than upper thresh. Can pass None to either to ignore.
+    """
+
     ## erase small rois:
-    labels_to_erase = [label for label in np.unique(labeled_led_img) if (len(np.where(labeled_led_img==label)[0]) < led_size_thresh[0] and label > 0) ]
-    for erase in labels_to_erase:
-        print('Erasing extraneous label #%d based on too small size...' % erase)
-        labeled_led_img[labeled_led_img==erase] = 0
+    if lower_size_thresh:
+        labels_to_erase = [label for label in np.unique(labeled_led_img) if (len(np.where(labeled_led_img==label)[0]) < lower_size_thresh and label > 0) ]
+        for erase in labels_to_erase:
+            print('Erasing extraneous label #%d based on too small size...' % erase)
+            labeled_led_img[labeled_led_img==erase] = 0
     
-    labels_to_erase = [label for label in np.unique(labeled_led_img) if (len(np.where(labeled_led_img==label)[0]) > led_size_thresh[1] and label > 0) ]
-    for erase in labels_to_erase:
-        print('Erasing extraneous label #%d based on too large size...' % erase)
-        labeled_led_img[labeled_led_img==erase] = 0
+    ## erase large rois
+    if upper_size_thresh:
+        labels_to_erase = [label for label in np.unique(labeled_led_img) if (len(np.where(labeled_led_img==label)[0]) > upper_size_thresh and label > 0) ]
+        for erase in labels_to_erase:
+            print('Erasing extraneous label #%d based on too large size...' % erase)
+            labeled_led_img[labeled_led_img==erase] = 0
 
     # Relabel 
     labeled_led_img = relabel_labeled_leds(labeled_led_img)
@@ -177,6 +222,10 @@ def extract_roi_events(labeled_led_img, led_labels, sorting, frame_data_chunk, m
         led_y = np.where(labeled_led_img==led_labels[sorting[i]])[1]
         led = frame_data_chunk[:,led_x,led_y].mean(axis=1) #on/off block signals
     
+
+        # Detect large changes in LED ROIs to detect on/off events
+        # NB: led_event_thresh refers to the np.diff'd signal!
+
         # If using avi, the range is pretty small, so use otsu to pick a good dividing number, then simplify to 0 or 1.
         if movie_type == 'avi':
             led_on_thresh = threshold_otsu(led)
@@ -184,7 +233,10 @@ def extract_roi_events(labeled_led_img, led_labels, sorting, frame_data_chunk, m
             led_event_thresh = 0
         elif movie_type == 'mkv':
             detection_vals = led
-            led_event_thresh = 2e4
+            led_event_thresh = 2e4  # ie, a change of 2e4 or greater
+        elif movie_type == 'basler':
+            detection_vals = led
+            led_event_thresh = 100  # ie, a change of 100 or greater. basler is uint8, LEDs go from ~250 to ~50.
 
         led_on = np.where(np.diff(detection_vals) > led_event_thresh)[0]   #rise indices
         led_off = np.where(np.diff(detection_vals) < (-1*led_event_thresh))[0]   #fall indices
@@ -202,18 +254,59 @@ def check_led_order(leds, num_leds):
     reverse = 0
     num_events_per_led = np.sum(leds!=0, axis=1)
     max_event_idx = np.where(num_events_per_led == np.max(num_events_per_led))[0]
-    if max_event_idx == 0:
-        reverse = 1
-    elif max_event_idx == (num_leds-1):
-        pass
-    elif len(max_event_idx) > 1:
+    
+    # Catch rare case where two LEDs share max event count
+    if len(max_event_idx)>1:
         if (0 in max_event_idx) and not ((num_leds-1) in max_event_idx):
             reverse = 1
         elif ((num_leds-1) in max_event_idx) and not (0 in max_event_idx):
             pass
+        elif (0 in max_event_idx) and ((num_leds-1) in max_event_idx):
+            Warning('First / last LEDs had same num events. Cannot validate LED order!')
         else:
             Warning('Multiple max events in LEDs and was not first or last in sort!')
+    elif max_event_idx == 0: # Deal with usual case
+        reverse = 1
+    elif max_event_idx == (num_leds-1):
+        pass
+        
     return reverse
+
+def split_largest_region(labeled_img, thickness = 10):
+
+    # Find region to split
+    rp = regionprops(labeled_img)
+    # max_idx = np.argmax([rg.area for rg in rp])
+    max_idx = np.argmax([rg.eccentricity for rg in rp])
+
+    # Split the region along its minor axis (ie into 2 LEDs):
+
+    # Find the minor axis endpoints
+    center = rp[max_idx].centroid
+    dx = np.cos(rp[max_idx].orientation) * rp[max_idx].minor_axis_length 
+    dy = np.sin(rp[max_idx].orientation) * rp[max_idx].minor_axis_length
+    p1 = (center[0]+dy, center[1]-dx)
+    p1_int = tuple(map(int,p1))
+    p2 = (center[0]-dy, center[1]+dx)
+    p2_int = tuple(map(int,p2))
+
+    # Draw the minor axis with some thickness
+    dx_thickness = np.sin(rp[max_idx].orientation) * thickness
+    dy_thickness = np.cos(rp[max_idx].orientation) * thickness
+    r1 = (p1[0]-dy_thickness, p1[1]-dx_thickness)  # 1,2,3,4 starting top left moving clockwise
+    r4 = (p1[0]+dy_thickness, p1[1]+dx_thickness)
+    r2 = (p2[0]-dy_thickness, p2[1]-dx_thickness)
+    r3 = (p2[0]+dy_thickness, p2[1]+dx_thickness)
+    p = polygon((r1[0], r2[0], r3[0], r4[0]), (r1[1], r2[1], r3[1], r4[1]))  # the minor axis with thickness
+    
+    # Split the image
+    labeled_img[p] = 0
+
+    # Relabel (the "relabel_labeled_leds()" function just remaps, it doesn't look for duplicated regions, so need to use ndi.label)
+    filled_img = (labeled_img>0).astype('uint8')
+    labeled_img, num_features = ndi.label(filled_img)
+    
+    return p,labeled_img
 
 
 def get_led_data_with_stds(frame_data_chunk, movie_type, num_leds = 4, chunk_num=0, led_loc=None,
@@ -227,12 +320,6 @@ def get_led_data_with_stds(frame_data_chunk, movie_type, num_leds = 4, chunk_num
     
     """
 
-    # Set up threshold for MKV
-    if movie_type == 'mkv':
-        led_thresh = 2e4
-    elif movie_type == 'avi':
-        led_thresh = None  # dynamically determine below with otsu
-    
     # Flip frames if requested
     if flip_horizontal:
         print('Flipping image horizontally')
@@ -250,17 +337,6 @@ def get_led_data_with_stds(frame_data_chunk, movie_type, num_leds = 4, chunk_num
     
     # If too many features, try a series of cleaning steps. Labeled_leds has 0 for background, then 1,2,3...for ROIs of interest
 
-    # 
-    # first try thresholding again, excluding very low values
-    # if num_features > num_leds:
-    #     print('Too many features, using second thresholding step...')
-    #     thresh2 = threshold_otsu(thresh_px[thresh_px > 5])
-    #     thresh_px[thresh_px < thresh2] = 0
-    #     edges = canny(thresh_px/255.) ## find the edges
-    #     filled_image = ndi.binary_fill_holes(edges) ## fill its edges
-    #     labeled_leds, num_features = ndi.label(filled_image) ## get the clusters
-    #     # plot_video_frame(labeled_leds,'%s/frame_%d_led_labels_secondThreshold.png' % (save_path,chunk_num))
-
     # If still too many features, check for location parameter and filter by it
     if (num_features > num_leds) and led_loc:
         print('Too many features, using provided LED position...')
@@ -272,15 +348,26 @@ def get_led_data_with_stds(frame_data_chunk, movie_type, num_leds = 4, chunk_num
     # If still too many features, remove small ones
     if (num_features > num_leds):
         print('Oops! Number of features (%d) did not match the number of LEDs (%d)' % (num_features,num_leds))
-        size_thresh = (20,100)  # min,max
-        labeled_led_img = clean_by_size(labeled_led_img, size_thresh)
+        if movie_type == 'mkv' or movie_type == 'avi':
+            labeled_led_img = clean_by_size(labeled_led_img, lower_size_thresh=20, upper_size_thresh=100)
+        elif movie_type == 'basler':
+            # check size of blob in 33000, 36000. Threshold needs to be above that!
+            labeled_led_img = clean_by_size(labeled_led_img, lower_size_thresh=500, upper_size_thresh=None)  # sometimes leds are merged so dont remove large ones
     
+    # Recompute num features (minus 1 for background)
+    num_features = len(np.unique(labeled_led_img)) - 1
 
+    # If using Basler, sometimes two LEDs get merged. If there are too few LEDs, take the biggest one, fit an ellipse, and cut it in half along its minor axis.
+    if (num_features < num_leds) and movie_type == 'basler':
+        print(f'Oops! Too few features {num_features} in Basler frame, trying to split largest feature')
+        split_line, labeled_led_img = split_largest_region(labeled_led_img, thickness = 10)
+
+    
     # Show led labels for debugging
     image_to_show = np.copy(labeled_led_img)
     # for i in range(1,5):
     #     image_to_show[labeled_leds==(sorting[i-1]+1)] = i
-    plot_video_frame(image_to_show, 200, '%s/frame_%d_led_labels_preEvents.png' % (save_path,chunk_num) )
+    plot_video_frame(image_to_show, 200, f'{save_path}/{movie_type}_frame_{chunk_num}_led_labels_preEvents.png')
 
     led_labels = [label for label in np.unique(labeled_led_img) if label > 0 ]
     assert led_labels == sorted(led_labels)  # note that these labels aren't guaranteed only the correct ROIs yet... but the labels should be strictly sorted at this point.
@@ -330,7 +417,7 @@ def get_led_data_with_stds(frame_data_chunk, movie_type, num_leds = 4, chunk_num
         tmp_idx_to_update.append(image_to_show == led_labels[sorting[i]])
     for i in range(len(sorting)):
         image_to_show[tmp_idx_to_update[i]] = (i+1)
-    plot_video_frame(image_to_show, 200, '%s/frame_%d_sort_order_postEvents.png' % (save_path,chunk_num) )
+    plot_video_frame(image_to_show, 200, '%s/%s_frame_%d_sort_order_postEvents.png' % (save_path, movie_type, chunk_num) )
 
     return leds
     
