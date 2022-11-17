443420683f7a36c7bbbd0a96466d400577a636f8
Jonah Pearl <jonahpearl@g.harvard.edu>
2022-11-17 14:08:30 -0500

    update cli

--- moseq2_ephys_sync/tests/test_cli.py
+++ moseq2_ephys_sync/tests/test_cli.py
@@ -9,7 +9,7 @@ def test_entrypoint():
 
 def test_cli_with_workflow_names():
     PATH_TO_TEST_DATA = '/n/groups/datta/Jonah/moseq2-ephys-sync/test_data/ino_basler_test/'
-    os.system(f'moseq2_ephys_sync -i {PATH_TO_TEST_DATA} -s1 arduino -s2 basler_bonsai --pytesting')
+    os.system(f'moseq2_ephys_sync {PATH_TO_TEST_DATA} -s1 arduino -s2 basler_bonsai --pytesting')
 
     matches = np.load('./tests/tmp_matches.npy')
     assert len(matches) > 0
@@ -23,7 +23,7 @@ def test_cli_with_paths_to_files():
     PATH_TO_TEST_DATA = '/n/groups/datta/Jonah/moseq2-ephys-sync/test_data/ino_basler_test/'
     arduino_fname = join(PATH_TO_TEST_DATA, '20220928_sync_test.txt')
     basler_data = join(PATH_TO_TEST_DATA, 'basler_2022-09-28T15_15_49.csv')
-    os.system(f'moseq2_ephys_sync -i {PATH_TO_TEST_DATA} -s1 {arduino_fname} -s2 {basler_data} --s2-timescale-factor-log10 9 --pytesting')
+    os.system(f'moseq2_ephys_sync {PATH_TO_TEST_DATA} -s1 {arduino_fname} -s2 {basler_data} --s2-timescale-factor-log10 9 --pytesting')
 
     matches = np.load('./tests/tmp_matches.npy')
     assert len(matches) > 0
@@ -38,7 +38,7 @@ def test_cli_outputs_with_path_to_files():
     out_dir = './tmp'
     arduino_fname = join(PATH_TO_TEST_DATA, '20220928_sync_test.txt')
     basler_data = join(PATH_TO_TEST_DATA, 'basler_2022-09-28T15_15_49.csv')
-    os.system(f'moseq2_ephys_sync -i {PATH_TO_TEST_DATA} -s1 {arduino_fname} -s2 {basler_data}  -o {out_dir} --s2-timescale-factor-log10 9')
+    os.system(f'moseq2_ephys_sync {PATH_TO_TEST_DATA} -s1 {arduino_fname} -s2 {basler_data}  -o {out_dir} --s2-timescale-factor-log10 9')
 
     assert exists(join(PATH_TO_TEST_DATA, out_dir, 'basler_2022-09-28T15_15_49_from_20220928_sync_test.p'))
     assert exists(join(PATH_TO_TEST_DATA, out_dir, '20220928_sync_test_from_basler_2022-09-28T15_15_49.p'))
